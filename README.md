dd55b814cb41b93c8809b1a07a4d428973856ff3
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-07-05 03:54:13 -0400

    Added tat command to quickly create new sessions

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ My tmux configuration file
 ## Setup
 - Clone this repository (I like to clone such configurations in ~/.configs/)
 - Run `chomd +x config.sh`
-- Run `./config`
+- Run `. ./config`
+
+NOTE: Ensure you run `config.sh` preceded by a `.` so that the configurations are set in the same shell and not a forked
+child.
--- config.sh
+++ config.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
 
+# Add tat command to PATH
+# sudo cat > /bin/tat <<EOF
+# tmux new-session -A -s ${1:-temp}
+# EOF
+
+# sudo cat > /bin/tat <<EOF
+# tmux new-session -A -s ${1:-temp}
+# EOF
+
+# sudo chmod +x /bin/tat
+
+if ! grep -q "tat()" "$HOME/.bashrc"; then 
+cat >> ~/.bashrc << 'EOF'
+
+# Function to create tmux sessions
+tat() {
+    path_name="$(basename "$PWD" | tr . -)"
+    session_name=${1-$path_name}
+
+      if [ -z "$TMUX" ]; then
+        tmux new-session -As "$session_name"
+      else
+        if ! tmux has-session -t "=$session_name"; then
+          (TMUX='' tmux new-session -Ad -s "$session_name")
+        fi
+        tmux switch-client -t "$session_name"
+      fi
+}
+EOF
+
+source ~/.bashrc
+fi
+
 # Symlink the configuration to tmux's default search file
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
-tmux source-file ~/.tmux.conf
-tmux display-message "~/.tmux.conf reloaded"
+ln -s $(pwd)/tmux.conf ~/.tmux.conf &> /dev/null
+
+echo $TMUX
+
+#if [ -n "$TMUX" ]; then
+    tmux source-file ~/.tmux.conf
+    tmux display-message "~/.tmux.conf reloaded"
+#fi
 
